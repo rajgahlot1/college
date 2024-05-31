c62254a6ba78f238dee53b83070cddc72a1c4922
rajgahlot1 <rajcommando1.38@gmail.com>
2024-05-31 21:30:27 +0530

    h

--- src/Program.jsx
+++ src/Program.jsx
@@ -13,28 +13,30 @@ const Program = ({ title, subTitle }) => {
   };
   const Images=[proIcon1,proIcon2,proIcon3];
   const Images2= [program1,program2,program3]
+  const imgName= ['Graduation Degree','Masters Degree','Post Graduation'];
   return (
     <>
       <div className="d-flex align-items-center justify-content-center flex-column">
-        <h3 className="pt-5 text-center">{subTitle}</h3>
+        <h3 className="pt-md-5 pt-3 text-center">{subTitle}</h3>
         <p className="pt-2 text-center fs-1 fw-bold">{title}</p>
-        <div style={imgStyle} className="row pb-5 pt-5 ms-3 me-3 mb-3 ">
+        <div style={imgStyle} className="row pb-md-5 pt-md-5 ms-md-3 me-md-3 mb-md-3 ">
             {Images.map((val,ind)=>{
                 return(
                  <div className="col-sm-4 col-8 mb-2">
-                 <div className="position-relative">
-                   <img
+                 <div className="position-relative  img1">
+                   <img 
                      src={Images2[ind]}
-                     className="img-fluid mt-sm-auto mb-sm-auto mb-3 mt-3 img1"
+                     className="img-fluid mt-sm-auto mb-sm-auto mb-3 mt-3"
                      alt=""
                      style={{ zIndex: "1" }}
                    />
+                   <div className="position-absolute img2 d-flex align-items-center justify-content-center flex-column">
                    <img
                      src={val}
-                    
-                     className="position-absolute img-fluid img2"
+                     className="w-25 img-fluid"
                      alt=""
-                   />
+                   /><h2 className="fs-md-1 fs-5">{imgName[ind]}</h2>
+                   </div>
                  </div>
                </div>)
             })}
